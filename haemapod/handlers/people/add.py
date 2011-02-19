abac294cae2c4e9ba22bd191dc94cd0885501cb9
John Tantalo <john.tantalo@gmail.com>
2011-02-19 00:01:36 -0800

    distance

--- haemapod/handlers/people/add.py
+++ haemapod/handlers/people/add.py
@@ -4,6 +4,7 @@ def post(handler, response):
     return handler.redirect('/')
   user.name = handler.request.get('name')
   user.link = handler.request.get('link')
+  user.distance = handler.request.get('distance')
   user.put()
   handler.redirect('/people/add')
 
@@ -11,3 +12,9 @@ def get(handler, response):
   user = response.user = handler.current_user()
   if not user:
     return handler.redirect('/')
+  response.distance_choices = [
+    ("stay here", "here"),
+    ("drive", "drive"), 
+    ("fly", "fly"),
+    ("go anywhere", "anywhere"),
+  ]
