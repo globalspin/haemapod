2a73912ab2103410246c7fc01017eb88011957db
John Tantalo <john.tantalo@gmail.com>
2011-02-19 00:58:45 -0800

    create and edit event

--- haemapod/handlers/events/add.py
+++ haemapod/handlers/events/add.py
@@ -1,7 +1,19 @@
 from model import Event
 
 def post(handler, response):
-  pass
+  key = handler.request.get('key')
+  event = Event.get(key) if key else Event()
+  event.name = handler.request.get('name')
+  event.link = handler.request.get('link')
+  event.city = handler.request.get('city')
+  lat = handler.request.get('lat')
+  lon = handler.request.get('lon')
+  if lat and lon:
+    event.location = db.GeoPt(lat, lon)
+    event.update_location()
+  event.put()
+  handler.redirect('/events/add?key='+str(event.key()))
 
 def get(handler, response):
-  response.event = Event()
+  key = handler.request.get('key')
+  response.event = Event.get(key) if key else Event()
