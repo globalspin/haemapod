dd7a356344933a50817d6d96dc975809761a5d66
John Tantalo <john.tantalo@gmail.com>
2011-02-19 06:12:58 -0800

    rm extra route

--- haemapod/main.py
+++ haemapod/main.py
@@ -8,7 +8,6 @@ def application():
     ('/', RequestHandler.with_page('handlers.default')),
     ('/people/add', RequestHandler.with_page('handlers.people.add')),
     ('/people', RequestHandler.with_page('handlers.people.search')),
-    ('/people/([^/]*)$', RequestHandler.with_page('handlers.people.detail')),
     ('/people/proximity', RequestHandler.with_page('handlers.people.proximity')),
     ('/people/bounding_box', RequestHandler.with_page('handlers.people.bounding_box')),
     ('/people/upload', RequestHandler.with_page('handlers.people.upload')),
