3e3d0f4b1c3e120e467f7961a7d021f15bd684dc
John Tantalo <john.tantalo@gmail.com>
2011-02-18 22:11:19 -0800

    /people/add

--- haemapod/main.py
+++ haemapod/main.py
@@ -5,7 +5,8 @@ from request_handler import RequestHandler
 
 def application():
   return WSGIApplication([
-    ('/', RequestHandler.with_page('handlers.default'))
+    ('/', RequestHandler.with_page('handlers.default')),
+    ('/people/add', RequestHandler.with_page('handlers.people.add')),
   ], debug=True)
 
 def main():
