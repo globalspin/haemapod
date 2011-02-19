f2faf40ac7492c6730d8f38c2efc9be0bdad0ef7
John Tantalo <john.tantalo@gmail.com>
2011-02-19 08:41:12 -0800

    event search

--- haemapod/handlers/events/default.py
+++ haemapod/handlers/events/default.py
@@ -4,4 +4,18 @@ from google.appengine.ext import db
 MAX_RESULTS = 10
 
 def get(handler, response):
-  response.events = Event.all().fetch(MAX_RESULTS)
+  next = handler.request.get('next')
+  if next:
+    event = Event.get(next)
+    if not event: return
+    events = Event.all()\
+      .filter('name >=', event.name)\
+      .order('name')\
+      .fetch(MAX_RESULTS+1)
+  else:
+    events = Event.all()\
+      .order('name')\
+      .fetch(MAX_RESULTS+1)
+  response.events = events[:MAX_RESULTS]
+  if len(events) > MAX_RESULTS:
+    response.next = events[-1]
