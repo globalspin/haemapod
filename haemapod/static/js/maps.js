3067c642de31a29881462203e116d5d85bdd2e75
reshun <reshun@reshun.com>
2011-02-19 09:35:22 -0800

    Added: event handler methods

--- haemapod/static/js/maps.js
+++ haemapod/static/js/maps.js
@@ -4,6 +4,8 @@ Site.Maps = function (oArgs) {
   this.initialLat = oArgs.latitude || 38;
   this.initialLng = oArgs.longitude || -97;
   this.mapType = oArgs.map_type || 'ROADMAP';
+  this.aMarkers = [];
+  this.aCircles = [];
   $(this.setup.bind(this));
 }
 
@@ -35,8 +37,8 @@ Site.Maps.prototype.initialize = function () {
   this.map = new GM.Map($('#map_canvas').get(0), this.mapOptions);
   
   // listen for tiles loaded
-  GM.event.addListener(this.map, 'tilesloaded', oMap.listUsers.bind(this));
-  GM.event.addListener(this.map, 'tilesloaded', oMap.listEvents.bind(this));
+  GM.event.addListener(this.map, 'tilesloaded', this.listUsers.bind(this));
+  GM.event.addListener(this.map, 'tilesloaded', this.listEvents.bind(this));
 }
 
 Site.Maps.prototype.getBoundingBoxCords = function (forReq) {
@@ -80,15 +82,15 @@ Site.Maps.prototype.listenServerServerResponse = function (oResponse) {
 
 Site.Maps.prototype.listUsers = function () {
   // load people
-  this.queryServer('/people/bounding_box', this.getBoundingBoxCords('forReq'), 'addItemCircle');
+  this.queryServer('/people/bounding_box', this.getBoundingBoxCords('forReq'), 'addItemUser');
 }
 
 Site.Maps.prototype.listEvents = function () {
   // load people
-  this.queryServer('/events/bounding_box', this.getBoundingBoxCords('forReq'), 'addItemMarkers');
+  this.queryServer('/events/bounding_box', this.getBoundingBoxCords('forReq'), 'addItemEvent');
 }
 
-Site.Maps.prototype.addItemMarkers = function (oResponse) {
+Site.Maps.prototype.addItemEvent = function (oResponse) {
   var oItems = oResponse.users || oResponse.events;
   $.each(oItems, function(_, oItem){
     var latLng = new GM.LatLng(oItem.lat, oItem.lng);
@@ -98,14 +100,15 @@ Site.Maps.prototype.addItemMarkers = function (oResponse) {
       icon: this.markerImage,
       title: oItem.name
     });
+    this.aMarkers.push(marker);
   }.bind(this));
 }
 
-Site.Maps.prototype.addItemCircle = function (oResponse) {
+Site.Maps.prototype.addItemUser = function (oResponse) {
   var oItems = oResponse.users || oResponse.events;
   $.each(oItems, function(_, oItem){
     var latLng = new GM.LatLng(oItem.lat, oItem.lng);
-    var marker = new GM.Circle({
+    var circle = new GM.Circle({
       center: latLng,
       map: this.map,
       radius: 50000,
@@ -115,11 +118,81 @@ Site.Maps.prototype.addItemCircle = function (oResponse) {
       strokeColor: '#000',
       strokeOpacity: 0.65,
     });
+    this.aCircles.push(circle);
   }.bind(this));
 }
 
-Site.Maps.prototype.getPeopleNearPoint = function (oCords) {
-  console.log(oCords);
+Site.Maps.prototype.highlightUser = function (user) {
+  $.each(this.aCircles, function(_, item) {
+    item.setOptions({
+      radius: 65000,
+      fillOpacity: 0.65,
+      fillColor: '#3A2FF9',
+      strokeColor: '#3A2FF9',
+      strokeOpacity: 1,
+      zIndex: 100
+    });
+  }.bind(this));
+}
+
+Site.Maps.prototype.highlightEvent = function (event) {
+  event = 'test';
+  $.each(this.aMarkers, function(_, item) {
+    console.log(event);
+    item.setMap(null);
+  }.bind(this));
+}
+
+Site.Maps.prototype.newUserAdded = function (user, events) {
+  // $.each(this.aCircles, function(_, item) {
+  //   item.setMap(this.map);
+  // }.bind(this));
+}
+
+Site.Maps.prototype.newEventAdded = function (event, users) {
+  // $.each(this.aCircles, function(_, item) {
+  //   item.setMap(this.map);
+  // }.bind(this));
+}
+
+Site.Maps.prototype.resetUsers = function () {
+  $.each(this.aCircles, function(_, item) {
+    item.setOptions({
+      radius: 50000,
+      fillOpacity: 0.8,
+      fillColor: '#C42816',
+      strokeWeight: 1,
+      strokeColor: '#000',
+      strokeOpacity: 0.65,
+      zIndex: 10
+    });
+    item.setMap(this.map); // Show the user
+  }.bind(this));
+}
+
+Site.Maps.prototype.resetEvents = function () {
+  $.each(this.aMarkers, function(_, item) {
+    item.setMap(this.map); // Show the event
+  }.bind(this));
+  
+  this.showUsers();
+}
+
+Site.Maps.prototype.resetMap = function () {
+  this.resetUsers();
+  this.resetEvents();
+}
+
+Site.Maps.prototype.showUsers = function () {
+  $.each(this.aCircles, function(_, item) {
+    item.setMap(this.map);
+  }.bind(this));
+}
+
+Site.Maps.prototype.hideUsers = function () {
+  $.each(this.aCircles, function(_, item) {
+    item.setMap(null);
+  }.bind(this));
 }
 
 var oMap = new Site.Maps();
