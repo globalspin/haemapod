1566e1503db482fe3d6aaabf58491aa3e8e74c7a
Chris Radcliff <chris.radcliff@gmail.com>
2011-02-18 23:37:00 -0800

    Added geomodel from Google Code.

--- haemapod/model/user.py
+++ haemapod/model/user.py
@@ -1,12 +1,13 @@
 from google.appengine.ext import db
 from google.appengine.api import users
+from geo.geomodel import GeoModel
 
-class User(db.Model):
+class User(GeoModel):
   user = db.UserProperty(required=True)
   email = db.StringProperty()
   name = db.StringProperty()
+  city = db.StringProperty()
   created = db.DateTimeProperty(auto_now_add=True)
-  location = db.StringProperty()
   distance = db.StringProperty(choices=set(["here", "drive", "fly", "anywhere"]))
   latitude = db.FloatProperty()
   longitude = db.FloatProperty()
