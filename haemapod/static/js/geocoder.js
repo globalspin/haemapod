a2942f5a3ce5e465cecc525a112bd9ea27e3f696
John Tantalo <john.tantalo@gmail.com>
2011-02-19 13:33:05 -0800

    event and people add forms

--- haemapod/static/js/geocoder.js
+++ haemapod/static/js/geocoder.js
@@ -1,13 +1,13 @@
 $(function () {
   $('#city').blur(function () {
-    $('#geocode_loading').show();
+    $('#geocode_loading').css({visibility: 'visible'});
     new google.maps.Geocoder().geocode({address: this.value}, function (r) {
-      if (!r || !r[0] || !r[0].geomtry) return;
+      if (!r || !r[0] || !r[0].geometry) return;
       var lon = r[0].geometry.location.Ba;
       var lat = r[0].geometry.location.za;
       $('#lon').val(lon);
       $('#lat').val(lat);
-      $('#geocode_loading').hide();
+      $('#geocode_loading').css({visibility: 'hidden'});
       $('[type=submit]').attr({disabled:false});
     });
   });
